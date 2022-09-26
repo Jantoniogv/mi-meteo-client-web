deef8ab43a4bae51feb2ecd8c268f7fdfbef9da4
Jose Antonio GV <jantoniogv85@gmail.com>
2022-09-26 20:31:02 +0200

    mod config.js

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 const PORT_SERVER = process.env.PORT || 3977;
 const SERVER_URL = "mi-meteo-api-production.up.railway.app" || "localhost";
 
-export const basePath = `http://${SERVER_URL}:${PORT_SERVER}/api`;
+export const basePath = `http://${SERVER_URL}/api`;
 export const apiVersion = "v1";
